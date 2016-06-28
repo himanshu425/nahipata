0e9c43366a4a8fc43ffab524256aa061c4679417
himanshu425 <himanshusangwan123@gmail.com>
2016-06-28 18:29:16 +0530

    Create Interface_CallBackNotify.java

---
+++ Interface_CallBackNotify.java
@@ -0,0 +1,7 @@
+package com.tcs.androidproject.Interface;
+
+
+public interface CallBackNotify {
+    public void onCallBack();
+    public void onCallBackCard(String a, String b, String c,String d);
+}
